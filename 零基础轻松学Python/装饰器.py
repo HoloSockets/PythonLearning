16c1edbd49bb2b39047e926daf33f4fa669595f0
HoloSockets <scalin@vip.qq.com>
2019-09-25 18:02:03 +0800

    '装饰器'

---
+++ 零基础轻松学Python/装饰器.py
@@ -0,0 +1,15 @@
+# -*- coding:utf-8 -*-
+
+
+def log(func):
+    def wrapper(*args, **kw):
+        print('call %s():' % func.__name__)
+        return func(*args, **kw)
+    return wrapper
+
+
+@log
+def now():
+    print('2019-9-25')
+
+now()
